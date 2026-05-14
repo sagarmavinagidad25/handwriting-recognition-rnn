a50fb653b3f7d9b75b5a9229705473eec40389e4
sagar <sagarmavinagidad25@gmail.com>
2026-05-15 00:18:15 +0530

    Added predict API

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 import os
 
 app = Flask(__name__)
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return jsonify({
+        "prediction": "S"
+    })
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
